79f8e46ebfbb68460863dea72b0c77f3c9b15826
JitendraBhumiwal <user@users-MacBook-Pro.local>
2014-04-25 10:42:51 +0530

    test

---
+++ ssfs/ssfs/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  ssfs
+//
+//  Created by user on 4/25/14.
+//  Copyright (c) 2014 user. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ ssfs/ssfs/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  ssfs
+//
+//  Created by user on 4/25/14.
+//  Copyright (c) 2014 user. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
